eef1dd085d5f96b64c7244284017ec2bb50d3400
taveirafelipe <taveira.felipe@gmail.com>
2022-08-31 15:46:08 -0300

    Exercicios aula

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
 //Crie a const para a frase aqui
+
+//a) Crie uma `const` no seu código para guardar a frase (com aspas e tudo);
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"."
+
+//b) Crie uma nova string a partir da primeira, trocando **verde** por **rosa**, e **azul** por **laranja**;
+const frase2 = frase.replace("verde","rosa").replace("azul","laranja")
+
+//c) Verifique se a nova string inclui **verde**, e se inclui **laranja**.
+console.log(frase2.replace("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR"))
